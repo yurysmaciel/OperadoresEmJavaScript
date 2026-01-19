cee2ad96bb2d0c6b090e6189de7fc37fc40d56cf
yurysmaciel <ymaciel789@gmail.com>
2026-01-19 20:05:44 -0300

    Metodos

--- 8-metodos-arrays.js
+++ 8-metodos-arrays.js
@@ -1,16 +1,16 @@
 // arrays
 
-const tecnologias = ['HTML', 'CSS', 'JAVASCRIPT', 'REACT', 'NODE.js']
-console.log(tecnologias)
+const tecnologias = ["HTML", "CSS", "JAVASCRIPT", "REACT", "NODE.js"];
+console.log(tecnologias);
 
 //função constutora
 
 const frutas = new Array("banana", "uva", "morango");
 console.log(frutas);
 
-console.log(frutas[frutas.length -1]);
+console.log(frutas[frutas.length - 1]);
 for (let i = 0; i < tecnologias.length; i++) {
-    console.log(tecnologias[i])
+  console.log(tecnologias[i]);
 }
 
 // Metodos de arrays
@@ -21,11 +21,11 @@ frutas.push("abacaxi");
 console.log(frutas);
 
 // pop() - remove o ultimo item do array
-tecnologias.pop()
-console.log(tecnologias)
+tecnologias.pop();
+console.log(tecnologias);
 
 // unshift() - Adiciona o item no inicio do array
-tecnologias.unshift('TailwindCSS');
+tecnologias.unshift("TailwindCSS");
 console.log(tecnologias);
 
 // shift() - Remove o item no inicio do array
@@ -35,13 +35,13 @@ console.log(tecnologias);
 
 // splice- Adiciona, remove ou substitui elementos em uma posição específica
 let palavras = [
-    ["sim", "não"],
-    ["positivo", "negativo"],
+  ["sim", "não"],
+  ["positivo", "negativo"],
 ];
 console.log(palavras[1][1]);
 
-let letras = ["A", "B", "C", "D", "E"]
-letras.splice(2, 1, "X", "Y", "Z", [1, 2, 3])
+let letras = ["A", "B", "C", "D", "E"];
+letras.splice(2, 1, "X", "Y", "Z", [1, 2, 3]);
 
 console.log(letras);
 
@@ -52,30 +52,32 @@ const doisCarros = carros.slice(2, 4);
 console.log(doisCarros);
 
 // Crie um array de objetos (um array de tenis - propriedades livre)
-const tenis = [{
+const tenis = [
+  {
     marca: "Nike",
-    tamanhos: [39, 40, 42,]
-},
-{
+    tamanhos: [39, 40, 42],
+  },
+  {
     marca: "Adidas",
-    tamanhos: [40, 41, 45]
-},
-{
+    tamanhos: [40, 41, 45],
+  },
+  {
     marca: "Mizuno",
-    tamanhos: [40, 41, 42, 43, 44]
-},
-{
+    tamanhos: [40, 41, 42, 43, 44],
+  },
+  {
     marca: "Lacoste",
-    tamanhos: [40, 42, 44]
-}]
+    tamanhos: [40, 42, 44],
+  },
+];
 console.log(tenis);
 
 console.log("1 - Adiciona um item no final");
 
 tenis.push({
-    marca: "Puma",
-    tamanhos: [42, 45]
-})
+  marca: "Puma",
+  tamanhos: [42, 45],
+});
 console.log(tenis);
 
 console.log("2 - Remove o item no inicio");
@@ -90,64 +92,68 @@ console.log(tenis);
 
 console.log("4 - Substitui um  item no meio e adiciona 2 novos tenis");
 
-tenis.splice(1, 0, {
+tenis.splice(
+  1,
+  0,
+  {
     marca: "Asics",
-    tamanhos: [39, 40, 42]
-}, 
-{
+    tamanhos: [39, 40, 42],
+  },
+  {
     marca: "Olympicus",
-    tamanhos: [39, 42, 45]
-})
+    tamanhos: [39, 42, 45],
+  },
+);
 console.log(tenis);
 
 // map() - crie um novo array aplicando uma função em cada elemento
 const meusNumeros = [2, 3, 4, 5, 6, 7];
 
 const numerosDobrados = meusNumeros.map((item) => {
-    return item * 2;
+  return item * 2;
 });
-console.log(numerosDobrados)
+console.log(numerosDobrados);
 
-// Exercicio 
+// Exercicio
 // triplique apenas os números pares usando map
 // impar numero % 2 !== 0;
 // par numero % 2 === 0;
 
-const algunsNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const algunsNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const triploNumeros = algunsNumeros.map((item) => {
-    if (item % 2 === 0){
-        return item * 3
-    }else if (item % 2 !== 0){
-        return item
-    }
-})
-console.log(triploNumeros)
+  if (item % 2 === 0) {
+    return item * 3;
+  } else if (item % 2 !== 0) {
+    return item;
+  }
+});
+console.log(triploNumeros);
 
 // filter() - Retorna um novo array apenas com os elementos que atendem a uma condição
 const outrosNumeros = [-10, -5, -4, -3, -2, 0, 1, 2, 4, 6];
 
-const maioresQueDois = outrosNumeros.filter((item)=>{
-    return item > 2
-})
-
-console.log(maioresQueDois)
-
-// Retorne apenas os numeros pares e dobrados
-const numerosParesDobrados = outrosNumeros.filter((item)=> {
-    return item % 2 === 0;
-}).map((item) => {
-    return item * 2;
+const maioresQueDois = outrosNumeros.filter((item) => {
+  return item > 2;
 });
 
-console.log(numerosParesDobrados)
+console.log(maioresQueDois);
+
+// Retorne apenas os numeros pares e dobrados
+const numerosParesDobrados = outrosNumeros
+  .filter((item) => {
+    return item % 2 === 0;
+  })
+  .map((item) => {
+    return item * 2;
+  });
+
+console.log(numerosParesDobrados);
 
 // find() - Retorne o primeiro elemento que satifaz a condição
-let numeros1 = [10, 20, 30, 40, 50]
+let numeros1 = [10, 20, 30, 40, 50];
 
-const encontrado = numeros1.find((item)=>{
-    return item > 30
-})
-console.log(encontrado)
-
-//
+const encontrado = numeros1.find((item) => {
+  return item > 30;
+});
+console.log(encontrado);
